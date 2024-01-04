792d058d016409cc13d60b3f377c89386cb6ec42
Haseeb Waheed Khan <haseebwaheedkhan@hotmail.com>
2024-01-04 14:18:58 +0500

    Created a Python file for my project Signoff-By-HaseebWaheedKhan

---
+++ Project/Customer review analyzer.py
@@ -0,0 +1,1 @@
+#Customer Review Analyzer: Empowering businesses with LangChain's linguistic prowess, our project swiftly deciphers and extracts valuable insights from customer reviews for informed decision-making.
